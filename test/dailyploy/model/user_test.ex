f5c5132dc246f400870207253bd8dd19616443f2
aonomike <aonomike@gmail.com>
2019-09-19 23:26:39 +0300

    Test create_user/1

--- test/dailyploy/model/user_test.ex
+++ test/dailyploy/model/user_test.ex
@@ -3,27 +3,42 @@ defmodule Dailyploy.Test.Model.UserTest do
   Contains tests on the User model
   """
   use Dailyploy.DataCase
-  alias Dailyploy.Model.User
+  alias Dailyploy.Model.User, as: UserModel
   alias Dailyploy.Model.Company
-  alias Dailyploy.Model.Workspace
+  alias Dailyploy.Model.Workspace, as: WorkspaceModel
   alias Dailyploy.Helper.User, as: UserHelper
+  alias Dailyploy.Schema.User
+  alias Dailyploy.Schema.Workspace
 
+
+  @workspace_params %{name: "Awesome Workspace", type: 0}
+  @user_params %{
+    name: "Awesome User",
+    email: "awesome@email.com",
+    password: "password",
+    password_confirmation: "password",
+    workspaces: [@workspace_params]
+  }
   @doc """
   list_users/0 returns a list of all  users in the database
+
+  ##Examples
+  iex> Dailyploy.Model.User.list_users()
+  [%Dailyploy.Model.User{}]
   """
   test "list_users/0" do
-    workspace_params = %{name: "Awesome Workspace", type: 0}
-    {:ok, workspace} = Workspace.create_workspace(workspace_params)
+    {:ok, user} = UserModel.create_user(@user_params)
+    assert length(UserModel.list_users()) == 1
+  end
 
-    user_params = %{
-      name: "Awesome User",
-      email: "awesome@email.com",
-      password: "password",
-      password_confirmation: "password",
-      workspaces: [workspace_params]
-    }
-
-    {:ok, user} = User.create_user(user_params)
-    assert length(User.list_users()) == 1
+  @doc """
+  create_user/1 returns a Struct of the created user
+  """
+  test "create_user/1" do
+    assert {:ok, user = %User{}} = UserModel.create_user(@user_params)
+    assert user.name == "Awesome User"
+    assert user.email == "awesome@email.com"
+    assert [%Workspace{} = workspace] = user.workspaces
+    assert workspace.name == "Awesome Workspace"
   end
 end
