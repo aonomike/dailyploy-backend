770967e1a6f916f76c602acfafc28020915e542b
aonomike <aonomike@gmail.com>
2019-09-24 00:03:32 +0300

    Test create project
    - Add test for create project(test still not fully implemented)

--- test/dailyploy/models/project_model_test.exs
+++ test/dailyploy/models/project_model_test.exs
@@ -2,19 +2,42 @@ defmodule Dailyploy.Test.Model.ProjectTest do
   @moduledoc """
   Contains test for the project model
   """
-  use Dailyploy.Datacase
+  use Dailyploy.DataCase
   alias Dailyploy.Schema.Project
-  alias Dailyploy.Model.Project
+  alias Dailyploy.Model.Project, as: ProjectModel
 
   @project_params %{
     name: "dailyploy",
-    start_date: "07-08-2019",
-    end_date: "10-10-2019",
+    start_date: ~D[2019-08-09],
+    end_date: ~D[2019-10-10],
     description: "Awesome",
     color_code: "red"
   }
+
+  @doc """
+  list_projects/0 returns a list of all projects in the database
+
+  ## Examples
+  iex> ProjectModel.list_projects()
+  [%Project{}]
+  
+  """
   test "list_projects/0" do
-    {:ok, project} = UserModel.create_project(@project_params)
-    assert length(UserModel.list_projects()) == 1
+    {:ok, project} = ProjectModel.create_project(@project_params)
+    assert length(ProjectModel.list_projects()) == 1
+  end
+
+  @doc """
+  create_project/1 receives a map and creates a project
+
+  ## Parameters
+  A map %{}
+
+  ##Examples
+  iex> ProjectModel.create_project(project)
+  {:ok, %Project{}}
+  """
+  test "create_project/1 receives a map and creates a project" do
+    {:ok, project} = ProjectModel.create_project(@project_params)
   end
 end
